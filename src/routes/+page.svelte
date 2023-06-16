f82e18f84daf4190147d770a9b6df62a377c77f4
LongarMD <mark.longar@gmail.com>
2023-06-16 12:21:41 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { i } from '@inlang/sdk-js'
+    
+    export let data: PageData;
+</script>
+
+<div>
+    {i('foo')}
+</div>
+<div>
+    text
+</div>

e6ece4ffb246ec7dd09a9e4a766008d1fd919968
LongarMD <mark.longar@gmail.com>
2023-06-16 12:22:13 +0200

    init

---
+++ inlang.config.js
@@ -0,0 +1,27 @@
+/**
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
+export async function defineConfig(env) {
+
+    const { default: pluginJson } = await env.$import(
+		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js",
+	)
+
+	const { default: sdkPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.6.5/dist/index.js",
+	)
+
+	return {
+		referenceLanguage: "en",
+		plugins: [
+			pluginJson({
+				pathPattern: "lang/{language}.json",
+			}),
+			sdkPlugin({
+				languageNegotiation: {
+					strategies: [{ type: "localStorage" }],
+				},
+			}),
+		],
+	}
+}
